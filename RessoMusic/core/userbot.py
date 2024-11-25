ffe965a8658b65611f7b7a1dd541245875be0da9
NAINCY <vishnu14sahil25love28@gmail.com>
2024-11-25 20:11:05 +0530

    Update userbot.py

--- RessoMusic/core/userbot.py
+++ RessoMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("FRIENDS_KI_MASTI_CLUB")
+                await self.one.join_chat("FRIENDS_KI_MASTI_CLUB")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
              try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("FRIENDS_KI_MASTI_CLUB")
+                await self.one.join_chat("FRIENDS_KI_MASTI_CLUB")
             except:
                 pass
             assistants.append(2)
