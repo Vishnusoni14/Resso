3ead5f16756400f3012c1a9b93022fd2ef40fa40
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2024-11-25 19:45:06 +0530

    Update __main__.py

--- RessoMusic/__main__.py
+++ RessoMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await AMBOTOP.start()
     try:
-        await AMBOTOP.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await AMBOTOP.stream_call("https://files.catbox.moe/r58ihg.mp4")
     except NoActiveGroupCall:
         LOGGER("RessoMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
@@ -50,12 +50,12 @@ async def init():
         pass
     await AMBOTOP.decorators()
     LOGGER("RessoMusic").info(
-        "BsdK Resso Music Start Hoga Be Ab"
+        " Moon Music Start Hoga Be Ab"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("RessoMusic").info("Stopping AMBOTOP Music Bot...")
+    LOGGER("RessoMusic").info("Stopping Moon Music Bot...")
 
 
 if __name__ == "__main__":
